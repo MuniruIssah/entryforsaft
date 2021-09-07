0211268455c12762b11fe514123df462d4beb6b0
Muniru Issah <imuniru@st.ug.edu.gh>
2021-09-07 00:33:06 +0000

    Additionas

--- src/components/RFHeader/components/LoginComponent/index.js
+++ src/components/RFHeader/components/LoginComponent/index.js
@@ -1,5 +1,8 @@
-import { Button, Drawer } from "antd";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Button, Divider, Drawer, Form, Input } from "antd";
 import React from "react";
+import "./styles.css";
 import { useState } from "react";
 
 const LoginCompoonent = () => {
@@ -15,7 +18,30 @@ const LoginCompoonent = () => {
         visible={showLoginDrawer}
         onClose={() => setShowLoginDrawer(false)}
       >
-        Hey im that drawer
+        {" "}
+        <div className="drawerWrapper">
+          <div className="loginWrapper">
+            <Button block size="large">
+              <FontAwesomeIcon icon={faGoogle} style={{ marginRight: 15 }} />{" "}
+              Sign In with Google
+            </Button>
+            <Divider>or</Divider>
+            <Form layout="vertical">
+              <Form.Item label="Email">
+                <Input size="large" type="email" />
+              </Form.Item>
+              <Form.Item label="Password">
+                <Input.Password size="large" />
+              </Form.Item>
+              <Form.Item>
+                <Button type="primary" size="large" className='myLoginButton' block>
+                  Login
+                </Button>
+              </Form.Item>
+            </Form>
+            <Button type="link">Go to Signup</Button>
+          </div>
+        </div>
       </Drawer>
     </>
   );
